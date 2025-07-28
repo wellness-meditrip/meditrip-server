c3eb3c402e5428652bf8f28af561a37b17d351f1
namduhus <kndh2914@gmail.com>
2025-07-29 02:50:25 +0900

    백엔드 서버용 설명 페이지

---
+++ page.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    services = [
+        {"name": "로그인/회원가입", "manager": "이규연"},
+        {"name": "계정", "manager": "이규연"},
+        {"name": "병원", "manager": "이규연"},
+        {"name": "리뷰", "manager": "남두현"},
+        {"name": "예약", "manager": "남두현"},
+        {"name": "의사", "manager": "남두현"},
+        {"name": "패키지", "manager": "남두현"},
+    ]
+    return render_template('index.html', services=services)
+
+if __name__ == '__main__':
+    app.run(debug=True)
