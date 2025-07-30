21be354bf353d122be0ebab100266d9f7cfe379d
namduhus <kndh2914@gmail.com>
2025-07-31 00:24:29 +0900

    [Fix] 코드수정

--- page.py
+++ page.py
@@ -8,10 +8,11 @@ def index():
         {"name": "로그인/회원가입", "manager": "이규연"},
         {"name": "계정", "manager": "이규연"},
         {"name": "병원", "manager": "이규연"},
+        {"name": "패키지", "manager": "이규연"},
         {"name": "리뷰", "manager": "남두현"},
         {"name": "예약", "manager": "남두현"},
         {"name": "의사", "manager": "남두현"},
-        {"name": "패키지", "manager": "남두현"},
+        {"name": "챗봇", "manager": "남두현"},
     ]
     return render_template('index.html', services=services)
 
