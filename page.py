6a71c1b443cecba38490e90e46ae9532ae1d08c4
namduhus <kndh2914@gmail.com>
2025-08-04 01:00:35 +0900

    hospitalAPI 명세서 링크 추가

--- page.py
+++ page.py
@@ -14,7 +14,7 @@ def index():
     services = [
         {"name": "로그인/회원가입", "manager": "이규연", "docs": f'<a href="{base_url}:8013/docs" target="_blank">로그인/회원가입API 문서</a>'},
         {"name": "계정", "manager": "이규연", "docs": "개발중"},
-        {"name": "병원", "manager": "이규연", "docs": "개발중"},
+        {"name": "병원", "manager": "이규연", "docs": f'<a href="{base_url}:8015/docs" target="_blank">병원 API 문서</a>'},
         {"name": "패키지", "manager": "이규연", "docs": "개발중"},
         {"name": "리뷰", "manager": "남두현", "docs": "개발중"},
         {"name": "예약", "manager": "남두현", "docs": "개발중"},
